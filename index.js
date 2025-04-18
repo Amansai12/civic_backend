f03a7cd9d2ca9c25d56251b6682a34af52a9d606
= <=>
2025-04-18 23:20:45 +0530

    added priority

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: ["http://localhost:5173","https://civic-wine.vercel.app"],
+        origin: process.env.FRONTEND_URL,
         credentials: true,
     })
 );
---
+++ prisma/migrations/20250418174913_added_priority/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Issue" ADD COLUMN     "priority" TEXT NOT NULL DEFAULT 'NORMAL';
